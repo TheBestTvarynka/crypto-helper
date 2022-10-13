6a28f4bfa2e060449678ecd373d5ba8bf2e270d7
Pavlo Myroniuk <pspos.developqkation@gmail.com>
2022-10-13 22:21:58 +0300

    web-app: output style improvement; mage go function generic (suitable for all algorithms); implement simple autoconvert for sha-like algorithms

--- web-app/src/index.js
+++ web-app/src/index.js
@@ -4,13 +4,63 @@ const CURRENT_ALGORITHM_INFO = 'current-algorithm-info';
 const CURRENT_ALGORITHM_IN = 'current-algorithm-in';
 const CURRENT_ALGORITHM_OUT = 'current-algorithm-out';
 
-console.log(wasm.simple_hash('crypto-helper'));
+try {
+  console.log(wasm.simple_hash('crypto-helper'));
+} catch(e) {
+  console.dir({ e });
+}
 
-const go = document.getElementById('go');
-go.addEventListener('click', () => {
-  const sha1In = document.getElementById('sha1-indata').value;
+const getAlgorithmFn = algorithm => algorithm;
 
-  const hash = wasm.sha1(sha1In);
-  const sha1Out = document.getElementById('sha1-outdata');
-  sha1Out.innerText = hash;
-});
+const getAlgorithm = () => document.getElementById('algorithm').selectedOptions[0].value;
+
+const collectData = algorithm => {
+  if (algorithm.startsWith('sha')) {
+    return [document.getElementById(`${algorithm}-indata`).value];
+  } else {
+    throw new Error('not implemented yet');
+  }
+};
+
+const setData = (algorithm, data) => {
+  if (algorithm.startsWith('sha')) {
+    document.getElementById(`${algorithm}-outdata`).innerText = data;
+  } else {
+    throw new Error('not implemented yet');
+  }
+};
+
+const go = () => {
+  console.log('go');
+  const algorithm = getAlgorithm();
+  const dataIn = collectData(algorithm);
+  try {
+    const dataOut = wasm[getAlgorithmFn(algorithm)](...dataIn);
+    setData(algorithm, dataOut);
+  } catch(e) {
+    console.error(e);
+  }
+};
+
+document
+  .getElementById('go')
+  .addEventListener('click', go);
+
+document
+  .getElementById('autoConvert')
+  .addEventListener('change', () => {
+    const algorithm = getAlgorithm();
+    if (document.getElementById('autoConvert').checked) {
+      if (algorithm.startsWith('sha')) {
+        const indata = document.getElementById(`${algorithm}-indata`);
+        indata.addEventListener('change', go);
+        indata.addEventListener('input', go);
+      }
+    } else {
+      if (algorithm.startsWith('sha')) {
+        const indata = document.getElementById(`${algorithm}-indata`);
+        indata.removeEventListener('change', go);
+        indata.removeEventListener('change', go);
+      }
+    }
+  })
