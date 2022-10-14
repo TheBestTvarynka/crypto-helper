f7ff95db0c277cb7d5cb3bf398ab63c7dc5f1f9c
Pavlo Myroniuk <pspos.developqkation@gmail.com>
2022-10-14 23:29:13 +0300

    web-app: add decrypt/encrypt switch (will work soon)

--- web-app/webpack.config.js
+++ web-app/webpack.config.js
@@ -1,6 +1,4 @@
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -12,19 +10,9 @@ module.exports = {
   mode: "development",
   plugins: [
     new CopyWebpackPlugin({ patterns: [ { from: 'src', to: '' }] }),
-    // new MiniCssExtractPlugin(),
-    // new HtmlWebpackPlugin(),
   ],
   experiments: {
     syncWebAssembly: true,
     asyncWebAssembly: true,
   }
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.css$/i,
-  //       use: [MiniCssExtractPlugin.loader, "css-loader"],
-  //     },
-  //   ],
-  // },
 };
