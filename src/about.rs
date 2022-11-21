f1afe4ad3b0974d58c4b24ea0cf03de6eff699a5
Pavlo Myroniuk <pspos.developqkation@gmail.com>
2022-11-21 20:29:26 +0200

    add About page; add routes (but they doesn't work)

---
+++ src/about.rs
@@ -0,0 +1,22 @@
+use yew::{html, Html, function_component, classes};
+
+#[function_component(About)]
+pub fn about() -> Html {
+    html! {
+        <div class={classes!("vertical", "about-container")}>
+            <span>{"Crypto-helper"}</span>
+            <span>{"The crypto-helper is an online app that helps to work with the diferent crypto algorithms:"}</span>
+            <vl>
+                <li>{"MD5"}</li>
+                <li>{"SHA1/SHA256/SHA512"}</li>
+                <li>{"Kerberos ciphers: AES128-CTS-HMAC-SHA1-96/AES256-CTS-HMAC-SHA1-96"}</li>
+                <li>{"Kerberos HMAC: HMAC-SHA1-96-AES128/HMAC-SHA1-96-AES256"}</li>
+                <li>{"RSA"}</li>
+                <li class={classes!("coming-soon")}>{"Coming soon: JWT/JWE debugger."}</li>
+            </vl>
+            <span>{"All computations perform on the client side. This tool never sends the data the any servers. Tip: if your input is not hex-encoded then you can use a "}<a href={"https://github.com/TheBestTvarynka/byte-formatter"}>{"byte-formatter"}</a>{" to transform input to the hex format."}</span>
+            <span>{"Authors: "}<a href={"https://github.com/TheBestTvarynka"}>{"Pavlo Myroniuk (@TheBestTvarynka)"}</a></span>
+            <span>{"GitHub: "}<a href={"https://github.com/TheBestTvarynka/crypto-helper"}>{"https://github.com/TheBestTvarynka/crypto-helper"}</a></span>
+        </div>
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod about;
 mod common;
 mod crypto_helper;
 mod footer;
@@ -6,17 +7,48 @@ mod notification;
 mod utils;
 
 use yew::{classes, function_component, html, Html};
+use yew_router::{Routable, BrowserRouter, Switch};
 
+use about::About;
 use crypto_helper::CryptoHelper;
 use footer::footer;
 use header::header;
+
+#[derive(Clone, Routable, PartialEq)]
+enum Route {
+    #[at("/")]
+    Home,
+    #[at("/crypto-helper")]
+    CryptoHelper,
+    #[at("/jwt")]
+    Jwt,
+    #[at("/about")]
+    About,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+}
+
+fn _switch(routes: Route) -> Html {
+    match routes {
+        Route::Home => html! { <CryptoHelper /> },
+        Route::CryptoHelper => html! { <CryptoHelper /> },
+        Route::Jwt => html!{ <span>{"Jw(t/e)"}</span> },
+        Route::About => html!{ <span>{"About"}</span> },
+        Route::NotFound => html!{ <span>{"Error: not found"}</span> },
+    }
+}
 
 #[function_component(App)]
 pub fn app() -> Html {
     html! {
         <div class={classes!("body")}>
             {header()}
-            <CryptoHelper />
+            // <BrowserRouter>
+            //     <Switch<Route> render={switch} />
+            // </BrowserRouter>
+            // <CryptoHelper />
+            <About />
             {footer()}
         </div>
     }
