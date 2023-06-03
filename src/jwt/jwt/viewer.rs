a0baaff1c3cd4f9eb94fd4953b86796657cf304c
Pavlo Myroniuk <the.best.tvarynka@gmail.com>
2023-06-03 20:50:50 +0300

    feat(jwt): viewer: rename variable rest -> leftover;

--- src/jwt/jwt/viewer.rs
+++ src/jwt/jwt/viewer.rs
@@ -15,7 +15,7 @@ pub fn jwt_viewer(props: &JwtViewerProps) -> Html {
     let header = props.jwt.raw_header.clone();
     let payload = props.jwt.raw_payload.clone();
     let signature = props.jwt.raw_signature.clone();
-    let rest = props.jwt.leftover.clone();
+    let leftover = props.jwt.leftover.clone();
 
     let clipboard = use_clipboard();
     let notifications = use_notification::<Notification>();
@@ -27,9 +27,9 @@ pub fn jwt_viewer(props: &JwtViewerProps) -> Html {
             <span class={classes!("jwt-payload")} onclick={copy_to_clipboard_with_notification(payload.clone(), clipboard.clone(), "Payload", notifications.clone())}>{payload}</span>
             <span class={classes!("jwt-dot")}>{"."}</span>
             <span class={classes!("jwt-signature")} onclick={copy_to_clipboard_with_notification(signature.clone(), clipboard.clone(), "Signature", notifications.clone())}>{signature}</span>
-            {if !rest.is_empty() {
+            {if !leftover.is_empty() {
                 html! {
-                    <span class={classes!("jwt-rest")} onclick={copy_to_clipboard_with_notification(rest.clone(), clipboard, "Token leftover", notifications)}>{rest}</span>
+                    <span class={classes!("jwt-rest")} onclick={copy_to_clipboard_with_notification(leftover.clone(), clipboard, "Token leftover", notifications)}>{leftover}</span>
                 }
             } else {
                 html! {}
