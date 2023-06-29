4a8e014cdc60a14d02d2473e733927c12b7a0458
Pavlo Myroniuk <the.best.tvarynka@gmail.com>
2023-06-29 23:17:34 +0300

    feat(asn1-page): init page;

---
+++ src/asn1.rs
@@ -0,0 +1,8 @@
+use yew::{function_component, html, Html};
+
+#[function_component(Asn1ParserPage)]
+pub fn asn1_parser_page() -> Html {
+    html! {
+        <div>{"asn1 parser"}</div>
+    }
+}
--- src/header.rs
+++ src/header.rs
@@ -9,6 +9,7 @@ pub fn header() -> Html {
         <header>
             <Link<Route> to={Route::CryptoHelper}>{"Crypto helper"}</Link<Route>>
             <Link<Route> to={Route::Jwt}>{"JWT"}</Link<Route>>
+            <Link<Route> to={Route::Asn1Parser}>{"Asn1 parser"}</Link<Route>>
             <Link<Route> to={Route::About}>{"About"}</Link<Route>>
         </header>
     }
