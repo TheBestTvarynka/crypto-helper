a1e2fef16f32c2d855125900bd5c45667221704f
Pavlo Myroniuk <the.best.tvarynka@gmail.com>
2023-07-05 22:24:34 +0300

    feat(crates): asn1-parser: length: implement write_len and len_size functions;

--- crates/asn1-parser/src/length.rs
+++ crates/asn1-parser/src/length.rs
@@ -2,10 +2,12 @@ use core::mem::size_of;
 use core::ops::Range;
 
 use crate::reader::Reader;
+use crate::writer::Writer;
 use crate::{Asn1Result, Error};
 
 const USIZE_LEN: usize = size_of::<usize>();
 
+/// Reads length from the reader
 pub fn read_len(reader: &mut Reader) -> Asn1Result<(usize, Range<usize>)> {
     let before = reader.position();
 
@@ -27,4 +29,27 @@ pub fn read_len(reader: &mut Reader) -> Asn1Result<(usize, Range<usize>)> {
     let after = reader.position();
 
     Ok((length, before..after))
+}
+
+/// Writes asn1 length into provided writer
+pub fn write_len(length: usize, writer: &mut Writer) -> Asn1Result<()> {
+    if length < 128 {
+        writer.write_byte(length.try_into()?)
+    } else {
+        let count_bytes: u8 = (USIZE_LEN - (length.leading_zeros() / 8) as usize).try_into()?;
+        writer.write_byte(count_bytes | 0x80)?;
+
+        let mut buf = [0; USIZE_LEN];
+        buf.copy_from_slice(&length.to_be_bytes());
+        writer.write_slice(&buf[USIZE_LEN - usize::from(count_bytes)..])
+    }
+}
+
+/// Returns how many bytes encoded length will take
+pub fn len_size(data_len: usize) -> usize {
+    if data_len < 128 {
+        1
+    } else {
+        1 + USIZE_LEN - (data_len.leading_zeros() / 8) as usize
+    }
 }
