71527309c724c370d9c45615c53e6923fed70a16
Jujumba <far77578@gmail.com>
2023-11-27 13:29:10 +0100

    argon2: add switch

--- src/crypto_helper/input/argon2.rs
+++ src/crypto_helper/input/argon2.rs
@@ -1,8 +1,6 @@
-use web_sys::HtmlInputElement;
-use yew::{classes, function_component, html, Callback, Html, Properties, TargetCast};
-use yew_notifications::{use_notification, Notification, NotificationType};
+use yew::{classes, function_component, html, Callback, Html, Properties};
 
-use crate::{crypto_helper::algorithm::Argon2Input as Argon2InputData, common::{build_byte_input, BytesFormat}};
+use crate::{crypto_helper::algorithm::{Argon2Input as Argon2InputData, Argon2Action}, common::{build_byte_input, BytesFormat, Switch}};
 
 #[derive(PartialEq, Properties, Clone)]
 pub struct Argon2InputProps {
@@ -12,17 +10,35 @@ pub struct Argon2InputProps {
 
 #[function_component(Argon2Input)]
 pub fn argon2_input(props: &Argon2InputProps) -> Html {
-    let data = props.input.data.clone();
+    let data: Vec<u8> = props.input.data.clone();
+
     let input_setter = props.argon2_input_setter.clone();
-    let action = props.input.action.clone();
+    let action: Argon2Action = props.input.action.clone();
     let setter = Callback::from(move |data| {
         input_setter.emit(Argon2InputData {
             action: action.clone(),
             data,
         })
     });
+
+    let input_setter = props.argon2_input_setter.clone();
+    let input = props.input.clone();
+    let on_switch = Callback::from(move |mode: bool| {
+        let Argon2InputData { data, ..} = input.clone();
+        input_setter.emit(Argon2InputData {
+            action: mode.into(),
+            data
+        })
+    });
+
+    let switch_state = bool::from(&props.input.action);
     html! {
         <div class={classes!("wide-input", "vertical")}>
+            <div class="horizontal">
+                <span class="total">{"hash"}</span>
+                <Switch id={"switch".to_string()} setter={on_switch} state={switch_state}/> 
+                <span class="total">{"verify"}</span>
+            </div>
             {build_byte_input(data, setter, Some(BytesFormat::Ascii), Some("argon2".into()))}
         </div>
     }
