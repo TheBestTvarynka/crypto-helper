023f837dcdd5832436a80d1408387c189cc0f70b
Pavlo Myroniuk <the.best.tvarynka@gmail.com>
2024-03-02 23:38:11 +0200

    feat(crypto-helper): diff: implement basic changes rendering;

--- src/diff/diff_viewer.rs
+++ src/diff/diff_viewer.rs
@@ -1,3 +1,5 @@
+use similar::DiffOp;
+use yew::virtual_dom::VNode;
 use yew::{function_component, html, Html, Properties};
 
 use super::DiffData;
@@ -7,11 +9,37 @@ pub struct DiffViewerProps {
     pub diff: DiffData,
 }
 
+fn render_changes(diff: &DiffData) -> Vec<VNode> {
+    let old = diff.original.as_slice();
+    let new = diff.changed.as_slice();
+    diff.changes.iter()
+        .flat_map(|op| match op {
+            DiffOp::Equal { old_index, len, new_index: _ } => vec![html! {
+                <span>{old[*old_index..*old_index + *len].iter().collect::<String>()}</span>
+            }],
+            DiffOp::Delete { old_index, old_len, new_index: _ } => vec![html! {
+                <span class="asn-bool-false">{old[*old_index..*old_index + *old_len].iter().collect::<String>()}</span>
+            }],
+            DiffOp::Insert { old_index: _, new_index, new_len } => vec![html! {
+                <span class="asn-bool-true">{new[*new_index..*new_index + *new_len].iter().collect::<String>()}</span>
+            }],
+            DiffOp::Replace { old_index, old_len, new_index, new_len } => vec![
+                html! {
+                    <span class="asn-bool-false">{old[*old_index..*old_index + *old_len].iter().collect::<String>()}</span>
+                },
+                html! {
+                    <span class="asn-bool-true">{new[*new_index..*new_index + *new_len].iter().collect::<String>()}</span>
+                },
+            ],
+        })
+        .collect()
+}
+
 #[function_component(DiffViewer)]
 pub fn diff_viewer(props: &DiffViewerProps) -> Html {
     html! {
         <div>
-            {format!("{:?}", props.diff.changes)}
+            {render_changes(&props.diff)}
         </div>
     }
 }
