d2d4d1e326fb65518db7b4f3be577add47fb3dcb
Pavlo Myroniuk <the.best.tvarynka@gmail.com>
2024-03-02 23:52:17 +0200

    feat(crypto-helper): diff: styling

--- src/diff/diff_viewer.rs
+++ src/diff/diff_viewer.rs
@@ -12,23 +12,41 @@ pub struct DiffViewerProps {
 fn render_changes(diff: &DiffData) -> Vec<VNode> {
     let old = diff.original.as_slice();
     let new = diff.changed.as_slice();
-    diff.changes.iter()
+    diff.changes
+        .iter()
         .flat_map(|op| match op {
-            DiffOp::Equal { old_index, len, new_index: _ } => vec![html! {
+            DiffOp::Equal {
+                old_index,
+                len,
+                new_index: _,
+            } => vec![html! {
                 <span>{old[*old_index..*old_index + *len].iter().collect::<String>()}</span>
             }],
-            DiffOp::Delete { old_index, old_len, new_index: _ } => vec![html! {
-                <span class="asn-bool-false">{old[*old_index..*old_index + *old_len].iter().collect::<String>()}</span>
+            DiffOp::Delete {
+                old_index,
+                old_len,
+                new_index: _,
+            } => vec![html! {
+                <span class="diff-remove">{old[*old_index..*old_index + *old_len].iter().collect::<String>()}</span>
             }],
-            DiffOp::Insert { old_index: _, new_index, new_len } => vec![html! {
-                <span class="asn-bool-true">{new[*new_index..*new_index + *new_len].iter().collect::<String>()}</span>
+            DiffOp::Insert {
+                old_index: _,
+                new_index,
+                new_len,
+            } => vec![html! {
+                <span class="diff-insert">{new[*new_index..*new_index + *new_len].iter().collect::<String>()}</span>
             }],
-            DiffOp::Replace { old_index, old_len, new_index, new_len } => vec![
+            DiffOp::Replace {
+                old_index,
+                old_len,
+                new_index,
+                new_len,
+            } => vec![
                 html! {
-                    <span class="asn-bool-false">{old[*old_index..*old_index + *old_len].iter().collect::<String>()}</span>
+                    <span class="diff-remove">{old[*old_index..*old_index + *old_len].iter().collect::<String>()}</span>
                 },
                 html! {
-                    <span class="asn-bool-true">{new[*new_index..*new_index + *new_len].iter().collect::<String>()}</span>
+                    <span class="diff-insert">{new[*new_index..*new_index + *new_len].iter().collect::<String>()}</span>
                 },
             ],
         })
@@ -38,7 +56,7 @@ fn render_changes(diff: &DiffData) -> Vec<VNode> {
 #[function_component(DiffViewer)]
 pub fn diff_viewer(props: &DiffViewerProps) -> Html {
     html! {
-        <div>
+        <div class="diff-changes-container">
             {render_changes(&props.diff)}
         </div>
     }
