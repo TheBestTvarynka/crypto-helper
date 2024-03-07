74962c4bfd44cf3f5f97fa58f681b850fed4ab65
Pavlo Myroniuk <the.best.tvarynka@gmail.com>
2024-03-08 00:43:36 +0200

    feat(crypto-helper): diff: improve diffs rendering. all multiline diff support;

--- src/diff/diff_viewer.rs
+++ src/diff/diff_viewer.rs
@@ -9,6 +9,23 @@ pub struct DiffViewerProps {
     pub diff: DiffData,
 }
 
+fn text_spans(chars: &[char], class: &'static str) -> Vec<VNode> {
+    chars
+        .iter()
+        .map(|c| {
+            if *c == '\n' {
+                html! {
+                    <span class="diff-new-line" />
+                }
+            } else {
+                html! {
+                    <span class={class}>{c}</span>
+                }
+            }
+        })
+        .collect()
+}
+
 fn render_changes(diff: &DiffData) -> Vec<VNode> {
     let old = diff.original.as_slice();
     let new = diff.changed.as_slice();
@@ -19,36 +36,27 @@ fn render_changes(diff: &DiffData) -> Vec<VNode> {
                 old_index,
                 len,
                 new_index: _,
-            } => vec![html! {
-                <span>{old[*old_index..*old_index + *len].iter().collect::<String>()}</span>
-            }],
+            } => text_spans(&old[*old_index..*old_index + *len], ""),
             DiffOp::Delete {
                 old_index,
                 old_len,
                 new_index: _,
-            } => vec![html! {
-                <span class="diff-remove">{old[*old_index..*old_index + *old_len].iter().collect::<String>()}</span>
-            }],
+            } => text_spans(&old[*old_index..*old_index + *old_len], "diff-remove"),
             DiffOp::Insert {
                 old_index: _,
                 new_index,
                 new_len,
-            } => vec![html! {
-                <span class="diff-insert">{new[*new_index..*new_index + *new_len].iter().collect::<String>()}</span>
-            }],
+            } => text_spans(&new[*new_index..*new_index + *new_len], "diff-insert"),
             DiffOp::Replace {
                 old_index,
                 old_len,
                 new_index,
                 new_len,
-            } => vec![
-                html! {
-                    <span class="diff-remove">{old[*old_index..*old_index + *old_len].iter().collect::<String>()}</span>
-                },
-                html! {
-                    <span class="diff-insert">{new[*new_index..*new_index + *new_len].iter().collect::<String>()}</span>
-                },
-            ],
+            } => {
+                let mut spans = text_spans(&old[*old_index..*old_index + *old_len], "diff-remove");
+                spans.extend_from_slice(&text_spans(&new[*new_index..*new_index + *new_len], "diff-insert"));
+                spans
+            }
         })
         .collect()
 }
